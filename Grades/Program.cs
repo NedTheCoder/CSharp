acfc183731b5f1e46fc3145360d37cadd543db75
NedTheCoder <nenadi@yahoo.com>
2019-01-05 15:45:50 -0800

    First commit

---
+++ Grades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook grades = new GradeBook();
+            grades.AddGrade(21);
+            grades.AddGrade(35.4f);
+            grades.AddGrade(34);
+
+            grades.ListGrades();
+            GradeStatistics stats = grades.CalculateStatistics();
+            Console.WriteLine("Average: " + stats.AverageGrade);
+            Console.WriteLine("Highest: " + stats.HighestGrade);
+            Console.WriteLine("Lowest: " + stats.LowestGrade);
+        }
+    }
+}
